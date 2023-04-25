d799bf2c0003a6e98cbc067210d9d6b228f22766
Shirahamah <hiropi1224s@gmail.com>
2023-04-26 00:00:27 +0900

    💄 update CustomCard

--- src/app/components/CustomCard.tsx
+++ src/app/components/CustomCard.tsx
@@ -1,11 +1,16 @@
 'use client';
 
 import { FC } from 'react';
-import { Card, Metric, Text } from '@tremor/react';
+import { Card, Flex, Metric, ProgressBar, Text } from '@tremor/react';
 
 export const CustomCard: FC = () => (
   <Card className='max-w-xs mx-auto'>
     <Text>Sales</Text>
-    <Metric>$ 34,743</Metric>
+    <Metric>$ 71,465</Metric>
+    <Flex className='mt-4'>
+      <Text>32% of annual target</Text>
+      <Text>$ 225,000</Text>
+    </Flex>
+    <ProgressBar percentageValue={32} className='mt-2' />
   </Card>
 );
