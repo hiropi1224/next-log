33e9ef2f868b598b51dbcb8fcfb96c788159d293
Shirahamah <hiropi1224s@gmail.com>
2023-04-29 15:23:47 +0900

    💄 blogsページ修正

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,38 +1,40 @@
-import { Fragment } from 'react';
+import { tv } from 'tailwind-variants';
 import { client } from '@/app/libs/client';
 import { BlogsResult } from '@/app/type';
 import { ContentCard } from '@/app/components/ContentCard';
-import { CustomCard } from '@/app/components/CustomCard';
 
 async function getData() {
   const res: BlogsResult = await client.get({
     endpoint: 'blogs',
+    queries: { limit: 10, offset: 0, orders: '-createdAt' },
   });
 
   return res;
 }
 
+const contents = tv({
+  slots: {
+    base: 'mx-auto max-w-screen-lg px-4 md:px-8',
+    area: 'm-2',
+  },
+});
+
+const { base, area } = contents();
+
 export default async function Blogs(): Promise<JSX.Element> {
   const data = await getData();
 
   return (
-    <div>
-      <main>
-        {data.contents.map((content) => (
-          <Fragment key={content.id}>
-            <CustomCard
-              title={content.title}
-              image={content.eyecatch.url}
-              createdAt={content.createdAt}
-            />
-            <ContentCard
-              title={content.title}
-              image={content.eyecatch.url}
-              createdAt={content.createdAt}
-            />
-          </Fragment>
-        ))}
-      </main>
-    </div>
+    <main className={base()}>
+      {data.contents.map((content) => (
+        <div key={content.id} className={area()}>
+          <ContentCard
+            title={content.title}
+            image={content.eyecatch.url}
+            createdAt={content.createdAt}
+          />
+        </div>
+      ))}
+    </main>
   );
 }
