916dc55aa8ce318ca478e4615166e68e980ebe7a
Shirahamah <hiropi1224s@gmail.com>
2023-05-08 23:53:21 +0900

    💄 dashboardTable作成

---
+++ src/app/components/dashboardTable.tsx
@@ -0,0 +1,55 @@
+'use client';
+import {
+  Table,
+  TableHead,
+  TableHeaderCell,
+  TableBody,
+  TableRow,
+  TableCell,
+  Title,
+  Text,
+  Card,
+} from '@tremor/react';
+import data from '@/app/data/record.json';
+import { formatDate } from '@/app/utils/formatDate';
+import { formatTime } from '@/app/utils/formatTime';
+import { metersToKilometers } from '@/app/utils/metersToKilometers';
+import { secondsToMinSecPerKm } from '@/app/utils/secondsToMinSecPerKm';
+
+export const DashboardTable: React.FC = () => {
+  return (
+    <Card>
+      <Title>パーソナルレコード(PR)</Title>
+      <Table className='mt-5'>
+        <TableHead>
+          <TableRow>
+            <TableHeaderCell>距離</TableHeaderCell>
+            <TableHeaderCell>タイム</TableHeaderCell>
+            <TableHeaderCell>平均ペース</TableHeaderCell>
+            <TableHeaderCell>日付</TableHeaderCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((item) => (
+            <TableRow key={item.type}>
+              <TableCell>
+                <Text>
+                  {typeof item.distance === 'string'
+                    ? item.distance
+                    : `${metersToKilometers(item.distance)}km`}
+                </Text>
+              </TableCell>
+              <TableCell>
+                <Text>{formatTime(item.duration)}</Text>
+              </TableCell>
+              <TableCell>
+                <Text>{secondsToMinSecPerKm(item.avgPace)}</Text>
+              </TableCell>
+              <TableCell>{formatDate(item.happenDay)}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </Card>
+  );
+};
