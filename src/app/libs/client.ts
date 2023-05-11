4666ee88f98bf56d3a46436d709bc82cb3f0cf6f
Shirahamah <hiropi1224s@gmail.com>
2023-05-11 22:20:25 +0900

    ♻️ microcmsClientにブログ取得処理を追加

--- src/app/libs/client.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'microcms-js-sdk';
-
-export const client = createClient({
-  serviceDomain: process.env.serviceDomain ?? '', // service-domain は XXXX.microcms.io の XXXX 部分
-  apiKey: process.env.apiKey ?? '',
-});
---
+++ src/app/libs/microcmsClient.ts
@@ -0,0 +1,58 @@
+import {
+  MicroCMSContentId,
+  MicroCMSListResponse,
+  createClient,
+} from 'microcms-js-sdk';
+
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSDate,
+} from 'microcms-js-sdk';
+
+//ブログの型定義
+export type Blog = {
+  id: string;
+  title: string;
+  content: string;
+  eyecatch?: MicroCMSImage;
+} & MicroCMSDate;
+
+export const client = createClient({
+  serviceDomain: process.env.serviceDomain ?? '', // service-domain は XXXX.microcms.io の XXXX 部分
+  apiKey: process.env.apiKey ?? '',
+});
+
+// ブログ一覧を取得
+export const getList = async (
+  queries?: MicroCMSQueries
+): Promise<MicroCMSListResponse<Blog>> => {
+  const listData = await client.getList<Blog>({
+    endpoint: 'blogs',
+    queries,
+  });
+
+  return listData;
+};
+
+// ブログの詳細を取得
+export const getDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+): Promise<
+  {
+    id: string;
+    title: string;
+    content: string;
+    eyecatch?: MicroCMSImage | undefined;
+  } & MicroCMSDate &
+    MicroCMSContentId
+> => {
+  const detailData = await client.getListDetail<Blog>({
+    endpoint: 'blogs',
+    contentId,
+    queries,
+  });
+
+  return detailData;
+};
