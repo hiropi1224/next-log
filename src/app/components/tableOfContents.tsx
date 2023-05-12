e317da008a13dbb55f06c3f18942c588cd8067e0
Shirahamah <hiropi1224s@gmail.com>
2023-05-12 23:23:46 +0900

    💄 サイドバーにstickyを追加

--- src/app/components/tableOfContents.tsx
+++ src/app/components/tableOfContents.tsx
@@ -1,11 +1,20 @@
 import { List, ListItem, Title, Text } from '@tremor/react';
 import Link from 'next/link';
+import { tv } from 'tailwind-variants';
 
 type Props = { toc: { text: string; id: string }[]; contentId: string };
 
+const toc = tv({
+  slots: {
+    base: 'sticky top-6',
+  },
+});
+
+const { base } = toc();
+
 export const TableOfContents: React.FC<Props> = ({ toc, contentId }) => {
   return (
-    <>
+    <div className={base()}>
       <Title>目次</Title>
       <List>
         {toc.map((item) => (
@@ -16,6 +25,6 @@ export const TableOfContents: React.FC<Props> = ({ toc, contentId }) => {
           </ListItem>
         ))}
       </List>
-    </>
+    </div>
   );
 };
