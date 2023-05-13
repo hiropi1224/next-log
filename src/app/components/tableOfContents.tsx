58b91b2d495a81c6631abac109aa283bb9b7d8e5
Shirahamah <hiropi1224s@gmail.com>
2023-05-14 00:27:44 +0900

    💄 目次のスタイル修正

--- src/app/components/tableOfContents.tsx
+++ src/app/components/tableOfContents.tsx
@@ -1,30 +1,70 @@
-import { List, ListItem, Title, Text } from '@tremor/react';
+import { Title } from '@tremor/react';
 import Link from 'next/link';
 import { tv } from 'tailwind-variants';
+import { TableOfContent } from '@/app/type';
 
-type Props = { toc: { text: string; id: string }[]; contentId: string };
+type Props = { toc: TableOfContent[]; contentId: string };
 
 const toc = tv({
   slots: {
     base: 'sticky top-6',
+    list: 'text-sm text-slate-500',
+    listItem:
+      'relative list-none pl-5 before:absolute before:left-0 before:top-1 before:h-2.5 before:w-2.5 before:inline-block before:rounded-full before:bg-sky-300',
   },
 });
 
-const { base } = toc();
+const { base, list, listItem } = toc();
 
 export const TableOfContents: React.FC<Props> = ({ toc, contentId }) => {
   return (
     <div className={base()}>
       <Title>目次</Title>
-      <List>
-        {toc.map((item) => (
-          <ListItem key={item.id}>
-            <Link href={`/blogs/${contentId}/#${item.id}`} scroll={false}>
-              <Text> {item.text}</Text>
-            </Link>
-          </ListItem>
-        ))}
-      </List>
+      <ul role='list' className={list()}>
+        {toc.map((item) => {
+          if (item.tag === 'h1') {
+            return (
+              <li key={item.id} className={listItem()}>
+                <Link href={`/blogs/${contentId}/#${item.id}`} scroll={false}>
+                  {item.text}
+                </Link>
+              </li>
+            );
+          } else if (item.tag === 'h2') {
+            return (
+              <ul
+                key={item.id}
+                className={listItem({
+                  class: 'm-3 before:h-1.5 before:w-1.5',
+                })}
+              >
+                <li>
+                  <Link href={`/blogs/${contentId}/#${item.id}`} scroll={false}>
+                    {item.text}
+                  </Link>
+                </li>
+              </ul>
+            );
+          } else if (item.tag === 'h3') {
+            return (
+              <ul key={item.id} className='m-3'>
+                <ul
+                  className={listItem({ class: 'm-3 before:h-1 before:w-1' })}
+                >
+                  <li>
+                    <Link
+                      href={`/blogs/${contentId}/#${item.id}`}
+                      scroll={false}
+                    >
+                      {item.text}
+                    </Link>
+                  </li>
+                </ul>
+              </ul>
+            );
+          }
+        })}
+      </ul>
     </div>
   );
 };
--- src/app/libs/renderToc.ts
+++ src/app/libs/renderToc.ts
@@ -1,6 +1,7 @@
 import * as cheerio from 'cheerio';
+import { TableOfContent } from '@/app/type';
 
-export const renderToc = (body: string): { text: string; id: string }[] => {
+export const renderToc = (body: string): TableOfContent[] => {
   const $ = cheerio.load(body);
   const headings = $('h1, h2, h3').toArray();
 
@@ -9,6 +10,7 @@ export const renderToc = (body: string): { text: string; id: string }[] => {
     //@ts-ignore
     text: data.children[0].data,
     id: data.attribs.id,
+    tag: data.tagName,
   }));
 
   return toc;
